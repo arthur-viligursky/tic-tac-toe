f732f828ad1386b6aef0d48f5072c6a481d041af
Arthur Vilgursky <arthur.v@apiko.com>
2022-06-29 17:51:30 +0300

    fixed GameService->getLastGame() method returning wrong game after ntp sync

--- www/app/Services/GameService.php
+++ www/app/Services/GameService.php
@@ -55,7 +55,7 @@ class GameService
         return Game
             ::query()
             ->whereBelongsTo($competition)
-            ->orderBy('created_at', 'desc')
+            ->orderBy('id', 'desc')
             ->first()
         ;
     }
